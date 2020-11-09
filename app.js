d2ed6803e683924bfee31c0795cb4ec9ffecf58e
Kamesh Kushwaha <52346625+kameshkushwaha@users.noreply.github.com>
2020-11-09 10:56:24 +0530

    get home

---
+++ app.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    //Drop Down Menu
+    $(".dropdown-trigger").dropdown();
+    //side nav
+    $('.sidenav').sidenav();
+    // fixed Action btn
+    $('.fixed-action-btn').floatingActionButton();
+})
